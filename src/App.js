996265ed4dffec5c08cc8fa9dbbc8f2b3d2ee2bc
Voodoo30fps <voodoo30fps@gmail.com>
2023-06-02 18:17:23 +0300

    layout, card design, form design, results design

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { useState } from 'react';
 function App() {
   const [weather, setWeather] = useState({});
   const [location, setLocation] = useState({
-    city: 'Pitesti',
-    country: 'Romania',
+    city: 'London',
+    country: 'England',
   });
   const [formVisible, setFormVisible] = useState(true);
 
@@ -22,38 +22,98 @@ function App() {
   const getWeather = async (e) => {
     e.preventDefault();
 
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${apiKey}`
-    );
-    const repsonseData = await response.json();
+    if (city.trim() === '' || country.trim() === '') {
+      alert('fill');
+    } else {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${apiKey}`
+      );
+      const repsonseData = await response.json();
 
-    setWeather(repsonseData);
+      setWeather(repsonseData);
+      setFormVisible(false);
+    }
   };
 
   return (
     <div className='main-container'>
-      <h1 className='center'>Weather App</h1>
+      <h1>Weather App</h1>
 
-      <section>
+      <section className={formVisible ? '' : 'hidden'}>
         <form onSubmit={getWeather}>
-          <input
-            type='text'
-            id='city'
-            name='city'
-            placeholder='City'
-            value={city}
-            onChange={onChange}
-          />
-          <input
-            type='text'
-            id='country'
-            name='country'
-            placeholder='Country'
-            value={country}
-            onChange={onChange}
-          />
+          <div className='form-field'>
+            <input
+              type='text'
+              id='city'
+              name='city'
+              value={city}
+              placeholder=' '
+              onChange={onChange}
+            />
+            <label htmlFor='city'>City</label>
+          </div>
+
+          <div className='form-field'>
+            <input
+              type='text'
+              id='country'
+              name='country'
+              value={country}
+              placeholder=' '
+              onChange={onChange}
+            />
+            <label htmlFor='country'>Country</label>
+          </div>
+
           <button type='submit'>Go</button>
         </form>
+      </section>
+
+      <section className={formVisible ? 'hidden' : 'center'}>
+        <div className='weather-location'>
+          <h3>
+            {weather.name} - {weather.sys.country}
+          </h3>
+
+          <div className='smaller'>
+            Lat: {weather.coord.lat} | Lon: {weather.coord.lon}
+          </div>
+        </div>
+
+        <div className='weather-main'>
+          <div>
+            <b>{weather.weather[0].main}</b>
+
+            <img
+              src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+              alt=''
+            />
+          </div>
+          {weather.weather[0].description}
+        </div>
+
+        <div className='weather-temperature'>
+          <div>
+            <b>Temperature: {weather.main.temp}F</b>
+          </div>
+          <div>Feels like: {weather.main.feels_like}F</div>
+          <div>
+            <div>Min: {weather.main.temp_min}F</div>
+            <div>Max: {weather.main.temp_max}F</div>
+          </div>
+        </div>
+
+        <div className='weather-wind'>
+          <div>Wind</div>
+          <div>
+            <div>
+              {weather.wind.speed} mph at {weather.wind.deg}°
+            </div>
+          </div>
+        </div>
+
+        <div>Humidity: {weather.main.humidity}</div>
+        <div>Pressure: {weather.main.pressure}</div>
       </section>
     </div>
   );
