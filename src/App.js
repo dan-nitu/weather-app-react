e0675a44e5279a41faaec46a6d8f4e7f08805fb5
Voodoo30fps <voodoo30fps@gmail.com>
2023-06-02 14:05:44 +0300

    initial react setup commit, added scss, react cleanup, data fetch from API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [weather, setWeather] = useState({});
+  const [location, setLocation] = useState({
+    city: 'Pitesti',
+    country: 'Romania',
+  });
+  const [formVisible, setFormVisible] = useState(true);
+
+  const { city, country } = location;
+
+  const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
+
+  const onChange = (e) => {
+    setLocation((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const getWeather = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${apiKey}`
+    );
+    const repsonseData = await response.json();
+
+    setWeather(repsonseData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+      <h1 className='center'>Weather App</h1>
+
+      <section>
+        <form onSubmit={getWeather}>
+          <input
+            type='text'
+            id='city'
+            name='city'
+            placeholder='City'
+            value={city}
+            onChange={onChange}
+          />
+          <input
+            type='text'
+            id='country'
+            name='country'
+            placeholder='Country'
+            value={country}
+            onChange={onChange}
+          />
+          <button type='submit'>Go</button>
+        </form>
+      </section>
     </div>
   );
 }
